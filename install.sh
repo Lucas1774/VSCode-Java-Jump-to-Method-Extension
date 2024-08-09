f273714035514339ef1b186ebc17d202288e8372
Lucas1774 <lucas.115@hotmail.com>
2024-08-09 23:02:17 +0200

    Install and configure webpack

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 set -e
+function cleanup {
+    echo "Press any key to exit."
+    read -n 1
+}
+trap cleanup ERR
+npx webpack --mode production --devtool hidden-source-map
+sed -i.bak 's#"main": "./extension.js"#"main": "./dist/extension.js"#' package.json
 vsce package
+mv package.json.bak package.json
 VSIX_FILE=$(ls *.vsix | head -n 1)
 code --install-extension "$VSIX_FILE"
+unzip -l "$VSIX_FILE"
 rm "$VSIX_FILE"
-echo "Press any key to exit."
-read -n 1
+cleanup
